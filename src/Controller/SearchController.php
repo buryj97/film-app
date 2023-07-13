da3cc58ce5110abf6a67ba067c9f142bcf6eea19
Jay Bury <buryj15@gmail.com>
2023-07-13 15:59:19 +0200

    test user log

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\FilmSearchType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
-    public function search(Request $request): Response
+    public function search(Request $request, UserRepository $repository): Response
     {
         /** @var User $user */
     $user = $this->getUser();
+    $users = $repository->findAll();
+        var_dump($users);
 
         $form = $this->createForm(FilmSearchType::class);
 
