e467a8162e5738dfceb64d35f1238bea56e7cf53
Jay Bury <buryj15@gmail.com>
2023-07-27 17:34:32 +0200

    modified error on login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
