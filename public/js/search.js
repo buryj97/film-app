28444521a1d601c1b3b76e63d255d15924768484
Jay Bury <buryj15@gmail.com>
2023-10-14 18:31:43 +0200

    moved country error

--- public/js/search.js
+++ public/js/search.js
@@ -16,9 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
 const form = document.getElementsByClassName("film_search")[0];
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  $(".card").remove();
-  cursor = "";
-  connectAPI();
+  if (selectedCheckboxes == null || country == null) {
+    const error = document.getElementById("error");
+    error.classList.remove("hidden");
+    form.focus();
+  } else {
+    $(".card").remove();
+    cursor = "";
+    connectAPI();
+  }
 });
 
 // ----------------------------------------------
@@ -67,68 +73,63 @@ function connectAPI() {
   const data = null;
 
   const xhr = new XMLHttpRequest();
-  if (selectedCheckboxes == null || country == null) {
-    const error = document.getElementById("error");
-    error.classList.remove("hidden");
-    form.focus();
-  } else {
-    xhr.withCredentials = true;
-    // create the query parameter string
-    let params =
-      "services=" +
-      encodeURIComponent(selectedCheckboxes.join(",")) +
-      "&country=" +
-      encodeURIComponent(country) +
-      "&show_original_language=" +
-      encodeURIComponent(originalLanguage) +
-      "&show_type=" +
-      encodeURIComponent("movie") +
-      "&keyword=" +
-      encodeURIComponent(keywords) +
-      "&genre=" +
-      encodeURIComponent(selectedOptions);
+  xhr.withCredentials = true;
+  // create the query parameter string
+  let params =
+    "services=" +
+    encodeURIComponent(selectedCheckboxes.join(",")) +
+    "&country=" +
+    encodeURIComponent(country) +
+    "&show_original_language=" +
+    encodeURIComponent(originalLanguage) +
+    "&show_type=" +
+    encodeURIComponent("movie") +
+    "&keyword=" +
+    encodeURIComponent(keywords) +
+    "&genre=" +
+    encodeURIComponent(selectedOptions);
 
-    // create parameter necessary for pagination
-    if (cursor !== "") {
-      params += "&cursor=" + encodeURIComponent(cursor);
-    }
-
-    // specify the API endpoint URL
-    const url = "https://streaming-availability.p.rapidapi.com/v2/search/basic";
-
-    // specify the HTTP request method and URL with query parameters
-    xhr.open("GET", url + "?" + params, true);
-
-    // set the content type of the request
-    xhr.setRequestHeader("Content-type", "application/octet-stream");
-
-    // set the RapidAPI headers
-    xhr.setRequestHeader(
-      "x-rapidapi-host",
-      "streaming-availability.p.rapidapi.com"
-    );
-    xhr.setRequestHeader(
-      "x-rapidapi-key",
-      "8d0b2a5100msh66455e428ed9568p197a01jsnb5d4ac778dbc"
-    );
-
-    // listen for the response
-    xhr.addEventListener("load", function () {
-      // check the status of the response
-      if (xhr.status === 200) {
-        // parse the response data
-        responseData = JSON.parse(xhr.responseText);
-        // pass response data to the function to create visual display
-        generateCards(responseData);
-      } else {
-        // handle errors
-        console.error("Error: " + xhr.status);
-      }
-    });
-    // send the request
-    xhr.send();
+  // create parameter necessary for pagination
+  if (cursor !== "") {
+    params += "&cursor=" + encodeURIComponent(cursor);
   }
+
+  // specify the API endpoint URL
+  const url = "https://streaming-availability.p.rapidapi.com/v2/search/basic";
+
+  // specify the HTTP request method and URL with query parameters
+  xhr.open("GET", url + "?" + params, true);
+
+  // set the content type of the request
+  xhr.setRequestHeader("Content-type", "application/octet-stream");
+
+  // set the RapidAPI headers
+  xhr.setRequestHeader(
+    "x-rapidapi-host",
+    "streaming-availability.p.rapidapi.com"
+  );
+  xhr.setRequestHeader(
+    "x-rapidapi-key",
+    "8d0b2a5100msh66455e428ed9568p197a01jsnb5d4ac778dbc"
+  );
+
+  // listen for the response
+  xhr.addEventListener("load", function () {
+    // check the status of the response
+    if (xhr.status === 200) {
+      // parse the response data
+      responseData = JSON.parse(xhr.responseText);
+      // pass response data to the function to create visual display
+      generateCards(responseData);
+    } else {
+      // handle errors
+      console.error("Error: " + xhr.status);
+    }
+  });
+  // send the request
+  xhr.send();
 }
+
 // ___________________________________________
 function generateCards(responseData) {
   const numResults = responseData.result.length;
