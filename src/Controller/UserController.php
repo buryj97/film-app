e936a1735223ba9a97a86efd357f672ad851bbb2
Jay Bury <buryj15@gmail.com>
2023-10-25 17:04:17 +0200

    modified htaccess for https redirect

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Form\SignUpType;
 use App\Repository\UserRepository;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
